c76fa59a259a6d7dcda36365cfd12908533e1a99
Kacper Grabiec <grabiec.kacper01@gmail.com>
2022-05-30 17:52:07 +0200

    feat: add MVVM structure

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MVVMShop
+namespace MVVMShop.View
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
